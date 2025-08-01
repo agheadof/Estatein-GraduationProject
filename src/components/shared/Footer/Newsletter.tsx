30c7f34c778f04bd595ff5bc39d2467cb24f48d4
Elin23 <elinshaia23@gmail.com>
2025-08-01 18:52:57 +0300

    Adding Footer

--- src/components/shared/Footer/Newsletter.tsx
+++ src/components/shared/Footer/Newsletter.tsx
@@ -2,7 +2,7 @@ import { MessageIcon, SendIcon } from "../../icons/FooterIcons";
 
 export function FooterNewsletter() {
   return (
-    <form className="space-y-2 w-full lg-custom:w-[305px] 2xl:w-[423px] border border-gray15 rounded-xl">
+    <form className="space-y-2 w-full lg-custom:w-[305px] 2xl:w-[423px] border border-gray15 rounded-xl ">
       <div className="relative">
         <MessageIcon
           className="absolute left-5 top-1/2 size-4 -translate-y-1/2 stroke-gray-light"
@@ -10,11 +10,11 @@ export function FooterNewsletter() {
         <input
           placeholder="Enter Your Email"
           autoComplete="email"
-          className="w-full px-12 py-3.5 rounded-xl"
+          className="w-full px-10 py-3.5 rounded-xl"
         />
         <button
           type="submit"
-          className="absolute right-1 top-1/2 grid size-10 -translate-y-1/2 place-items-center"
+          className="absolute right-1 top-1/2 size-10 -translate-y-1/2 cursor-pointer"
         >
           <SendIcon />
         </button>
