90a4c7aa84bc77398d98a6096d72d643bae5eeda
Elin23 <elinshaia23@gmail.com>
2025-08-02 22:49:53 +0300

    adding Loader & Scroll2Top

---
+++ src/components/ui/Scroll2Top.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import ArrowUpIcon from "../icons/ArrowUpIcons";
+import { scrollToTop } from "../../utlis/scrollToTop";
+
+const Scroll2Top = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY > 200) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener('scroll', toggleVisibility);
+    return () => window.removeEventListener('scroll', toggleVisibility);
+  }, []);
+
+
+  return (
+    <>
+      {isVisible && (
+        <div className="fixed bottom-6 right-6 z-50 group">
+          <button
+            onClick={scrollToTop}
+            className="w-[50px] h-[50px] rounded-full bg-neutral-900 border-none font-semibold flex items-center justify-center shadow-[0_0_0_4px_rgba(180,160,255,0.253)] cursor-pointer overflow-hidden transition-all duration-300 group-hover:w-[140px] group-hover:rounded-full group-hover:bg-purple70"
+          >
+            <ArrowUpIcon className="w-[12px] transition-transform duration-300 group-hover:-translate-y-[200%]" />
+            <span className="absolute  text-white text-[0px] group-hover:text-[13px] group-hover:bottom-auto transition-all duration-300">
+              Back to Top
+            </span>
+          </button>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Scroll2Top;
